dff3aca892d87b411a741358481613a1932e1f21
Jina Zhu <jinazhu87@gmail.com>
2020-02-10 22:53:42 -0800

    added ryan

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React from 'react';
+import $ from "jquery"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    gifs: []
+  }
+
+  fetchGifs() {
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=wkW1B4BR0fsNO7LFjglv4GffzOpU4pPk&limit=5");
+    xhr.done((data) => {
+      // arrow functions do not bind the this keyword to the execution context
+      // when you define a arrow function this will maintain its value at the global scope
+      // this is reference to the execution context
+      console.log(data)
+      this.setState({gifs: data.data}) 
+    });
+
+  }
+  // a component must return JXS
+  // JXS = html and javascript(stuff you can see)
+  // {name} =interpolation
+      // like jinja {{}}
+  //lifecycle are called automatic
+    componentDidMount() {
+      // only run once
+      this.fetchGifs()
+      //this is a reference to the class itself. Like a self 
+    }
+  render(){
+//render is called by itself
+    console.log(this.state.gifs)
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          hello Jina, You daaa bessst!
+          Learning how to query the Giphy API!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {this.state.gifs.map((gif) => {
+            return <img src={gif.images.downsized_medium.url}/>
+          })}
       </header>
     </div>
   );
+}
 }
 
 export default App;
